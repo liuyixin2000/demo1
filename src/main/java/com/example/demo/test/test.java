2e0362ac295fa1cda1a24739fcab280503936656
liuyixin <1049637680@qq.com>
2022-09-06 17:02:21 +0800

    master v2

--- src/main/java/com/example/demo/test/test.java
+++ src/main/java/com/example/demo/test/test.java
@@ -22,23 +22,18 @@ public class test {
      *@Data：2022/9/6 16:45,
      */
     /**
+     *@Description：v4
+     *@Author：LiuYiXin
+     *@Data：2022/9/6 16:59,
+     */
+    /**
+     *@Description：master v1
+     *@Author：LiuYiXin
+     *@Data：2022/9/6 17:01,
+     */
+    /**
      *@Description：hot-fix
      *@Author：LiuYiXin
      *@Data：2022/9/6 16:51,
-     */
-    /**
-     *@Description：hot-fix v3
-     *@Author：LiuYiXin
-     *@Data：2022/9/6 16:56,
-     */
-    /**
-     *@Description：
-     *@Author：LiuYiXin
-     *@Data：2022/9/6 16:55,
-     */
-    /**
-     *@Description：hot-fix v2
-     *@Author：LiuYiXin
-     *@Data：2022/9/6 16:55,
      */
 }
